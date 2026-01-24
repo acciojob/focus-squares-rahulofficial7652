b5b986b6a9e5c03c80245c5f7fbc518f0ac5861f
Rahul <rahulofficial7652@gmail.com>
2026-01-24 13:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+/* If the container HAS a hovered square, make all squares coffee */
+.container:has(.square:hover) .square {
+  background-color: #6F4E37;
+}
+
+/* Except the one actually being hovered */
+.square:hover {
+  background-color: #E6E6FA !important;
+}
