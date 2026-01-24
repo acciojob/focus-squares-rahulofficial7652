ea396f7c776d8653ea67832c8c117d9c288a10c2
Rahul <rahulofficial7652@gmail.com>
2026-01-24 13:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/* If the container HAS a hovered square, make all squares coffee */
-.container:has(.square:hover) .square {
-  background-color: #6F4E37;
-}
-
-/* Except the one actually being hovered */
-.square:hover {
-  background-color: #E6E6FA !important;
-}
+const inputField = document.getElementById("fname");
+inputField.onblur = () => {
+    inputField.value = inputField.value.toUpperCase();
+};
